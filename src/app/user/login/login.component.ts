a9437aa05938994b589279d84a05978b1801cf88
Cahyo <cahyo.adi.permono@gmail.com>
2022-01-29 19:57:01 +0700

    add login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,24 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  inSubmission = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please Wait!!';
+
+  constructor(private auth: AngularFireAuth) {}
 
   credentials = {
     email: '',
     password: '',
+  };
+
+  ngOnInit(): void {}
+
+  async login() {
+    this.inSubmission = true;
+    this.showAlert = true;
+    this.alertMsg = 'Please Wait!!';
+    this.alertColor = 'blue';
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error) {
+      console.error(error);
+      this.inSubmission = false;
+      this.showAlert = true;
+      this.alertMsg = 'Error Login';
+      this.alertColor = 'red';
+      return
+    }
+
+    this.showAlert = true;
+    this.alertMsg = 'Succesfully Login';
+    this.alertColor = 'green';
+    this.inSubmission = false;
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  login(){
-    console.log(this.credentials);
-  }
-
 }
